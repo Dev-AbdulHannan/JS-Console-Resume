d1bd036a24f90770d2febc19f7b02027f07089cf
Abdul Hannan <comsianabdulhannan@gmail.com>
2023-02-11 17:25:03 +0500

    complted lab

---
+++ app.js
@@ -0,0 +1,24 @@
+let Name='Abdul Hannan';
+let n=Name.toUpperCase();
+console.log("Name: "+n);
+
+
+displayPosition("Google","Software Engineer","I love Coding💖");
+
+console.log("Skills\n");
+displaySkills("javascript",true);
+displaySkills("Java",false);
+displaySkills("English",true);
+
+function displayPosition(companyname, jobtitle, description)
+{
+   console.log("Company Name: "+companyname ,"\nJob: "+jobtitle ,"\nDescription: "+description);
+}
+
+function displaySkills(skillName, boolean)
+{
+ if (boolean==true)
+ console.log("\n*BAM:"+skillName);
+   else if(boolean==false)
+ console.log("\n*"+skillName);
+}
